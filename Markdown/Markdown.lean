b72d6f0d553feb55178fd1b30e123d17310cd697
George McNinch <gmcninch@gmail.com>
2026-04-23 13:41:17 -0400

    append instance for TextItem

--- Markdown/Markdown.lean
+++ Markdown/Markdown.lean
@@ -56,6 +56,9 @@ def renderTextItems (items : List TextItem) : String :=
 
 end
 
+instance : Append Markdown.TextItem where
+  append x y := .seq [ x , y ]
+
 def renderTableCell (cell : TableCell) : String :=
   renderTextItems cell.content
 
